5c0f02930f6e94366375fbada4d957ca866a9960
Asitha Navaratne <asithan476@gmail.com>
2021-12-03 22:08:44 +0530

    UI format complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://archive.org/download/github.com-freeCodeCamp-chapter_-_2019-10-19_07-29-14/cover.jpg" alt="Profile picture" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5 ps-5">
+            <div class="ps-5"><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="ps-5"><strong>153</strong> posts</div>
+                <div class="ps-5"><strong>1.1k</strong> followers</div>
+                <div class="ps-5"><strong>832</strong> following</div>
             </div>
+            <div class="pt-4 ps-5 font-weight-bold">freeCodeCamp.org</div>
+            <div class="ps-5">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div class="ps-5"><a href="www.freecodecamp.org">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202012/chris-ried-ieic5Tq8YMk-unsplas_1200x768.jpeg?bEhcYQAShJnLf0Mtu4JYq8YzICfhz2rB&size=770:433" alt="Post" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.eschoolnews.com/files/2021/02/coding-platform-STEM-code.jpg" alt="Post" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://www.gettingsmart.com/wp-content/uploads/2017/06/Program-Code-Feature-Image.jpg" alt="Post" class="w-100">
         </div>
     </div>
 </div>
